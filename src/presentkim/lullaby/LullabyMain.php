7d08713be711ee2af74c0cc5a9de2c8515324c7d
PresentKim <qjrmckd@gmail.com>
2018-01-16 22:39:18 +0900

    Fix save lang in load() and save()

--- src/presentkim/lullaby/LullabyMain.php
+++ src/presentkim/lullaby/LullabyMain.php
@@ -76,8 +76,10 @@ class LullabyMain extends PluginBase{
         // load lang
         $langfilename = $dataFolder . 'lang.yml';
         if (!file_exists($langfilename)) {
-            Translation::loadFromResource($this->getResource('lang/eng.yml'));
-            Translation::save($langfilename);
+            $resource = $this->getResource('lang/eng.yml');
+            Translation::loadFromResource($resource);
+            stream_copy_to_stream($resource, $fp = fopen("{$dataFolder}lang.yml", "wb"));
+            fclose($fp);
         } else {
             Translation::load($langfilename);
         }
@@ -102,13 +104,7 @@ class LullabyMain extends PluginBase{
         $this->saveConfig();
 
         // save lang
-        $langfilename = $dataFolder . 'lang.yml';
-        if (!file_exists($langfilename)) {
-            Translation::loadFromResource($this->getResource('lang/eng.yml'));
-            Translation::save($langfilename);
-        } else {
-            Translation::load($langfilename);
-        }
+        Translation::save("{$dataFolder}lang.yml");
     }
 
     /**
